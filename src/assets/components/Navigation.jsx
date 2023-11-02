8b43bbe2198f55b908584864a965e701b6cf4728
Sebastián Barra <s.andresbarra@gmail.com>
2023-11-01 22:27:41 -0300

    icon change

--- src/assets/components/Navigation.jsx
+++ src/assets/components/Navigation.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FcHome } from "react-icons/fc";
 import { FcAddressBook } from "react-icons/fc";
+import { FcLikePlaceholder } from "react-icons/fc";
 import { Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
@@ -12,16 +13,20 @@ const Navigation = () => {
                 <Nav className="ml-auto">
                     <Nav.Item>
                         <Link to="/" className="nav-link text-white fw-medium fs-5">
-                            <FcHome className="mb-2 align-text-center" /> Home
+                            <FcHome className="mb-2 " /> Home
                         </Link>
                     </Nav.Item>
                     <Nav.Item>
                         <Link to="/Contact" className="nav-link text-white fw-medium fs-5">
-                            <FcAddressBook className="mb-2 align-text-center" /> Contact
+                            <FcAddressBook className="mb-2 " /> Contact
                         </Link>
                     </Nav.Item>
                 </Nav>
             </Navbar.Collapse>
+            <div className="text-white me-3" style={{ display: "flex", alignItems: "center" }}>
+  <FcLikePlaceholder className="fs-3 mb-2" />
+  <h6 className="fs-5">Happy Cake</h6>
+</div>
         </Navbar>
     );
 };
--- src/assets/views/HomePage.jsx
+++ src/assets/views/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
 return (
     <div className="text-center mt-5">
             <h1  className='fw-normal'>
-                Bienvenido a <strong>Premium Desserts</strong>
+                Bienvenido a <strong>Happy Cake</strong>
             </h1>
             <h3 className='fw-normal'>El lugar de los pasteles felices</h3>
             <img src="src/assets/img/logomain.png" alt="" />
